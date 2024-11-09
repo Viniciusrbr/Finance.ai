be098fdb71a023f4d62f9893ed3aec3949b16d65
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-11-09 11:48:24 -0300

    feat: add navbar components

---
+++ app/subscription/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../_components/navbar";
+
+export default function SubscriptionPage() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,4 +1,5 @@
 import AddTransactionButton from "../_components/add-transaction-button";
+import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionColumns } from "./_columns";
@@ -7,16 +8,19 @@ export default async function TransactionsPage() {
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div className="space-y-6 p-6">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-black">Transações</h1>
-        <AddTransactionButton />
-      </div>
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-black">Transações</h1>
+          <AddTransactionButton />
+        </div>
 
-      <DataTable
-        columns={transactionColumns}
-        data={JSON.parse(JSON.stringify(transactions))}
-      />
-    </div>
+        <DataTable
+          columns={transactionColumns}
+          data={JSON.parse(JSON.stringify(transactions))}
+        />
+      </div>
+    </>
   );
 }
