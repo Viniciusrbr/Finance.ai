0308139de02728af75e1e63b756e4c9d4cfc8f85
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-11-11 17:09:38 -0300

    feat: add componente de despesa por categoria

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,10 +4,10 @@ import { redirect } from "next/navigation";
 
 import Navbar from "../_components/navbar";
 import { getDashboard } from "../_data/get-dashboard";
+import ExpensesPerCategory from "./_components/expenses-per-category";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
-
 interface HomeProps {
   searchParams: {
     month: string;
@@ -40,6 +40,9 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
             <SummaryCards month={month} {...dashboard} />
             <div className="grid grid-cols-3 grid-rows-1 gap-6">
               <TransactionsPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
             </div>
           </div>
         </div>
--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -2,7 +2,7 @@ import { TransactionType } from "@prisma/client";
 
 import { db } from "@/app/_lib/prisma";
 
-import { TransactionPercentagePerType } from "./types";
+import { TotalExpensePerCategory, TransactionPercentagePerType } from "./types";
 
 export const getDashboard = async (month: string) => {
   const where = {
@@ -61,11 +61,32 @@ export const getDashboard = async (month: string) => {
       (Number(investmentsTotal || 0) / Number(transactionsTotal)) * 100,
     ),
   };
+
+  const totalExpensePerCategory: TotalExpensePerCategory[] = (
+    await db.transaction.groupBy({
+      by: ["category"],
+      where: {
+        ...where,
+        type: TransactionType.EXPENSE,
+      },
+      _sum: {
+        amount: true,
+      },
+    })
+  ).map((category) => ({
+    category: category.category,
+    totalAmount: Number(category._sum.amount),
+    percentageOfTotal: Math.round(
+      (Number(category._sum.amount) / Number(expensesTotal)) * 100,
+    ),
+  }));
+
   return {
     balance,
     depositsTotal,
     investmentsTotal,
     expensesTotal,
     typesPercentage,
+    totalExpensePerCategory,
   };
 };
