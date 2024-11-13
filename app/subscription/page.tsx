ab80f054be663e203f679e7e687d0af6ad1cfb23
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-11-13 15:00:20 -0300

    feat: stripe webhook para lidar com assinaturas

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,8 +1,9 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { CheckIcon, XIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 import Navbar from "../_components/navbar";
+import { Badge } from "../_components/ui/badge";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import AcquirePlanButton from "./_components/acquire-plan-button";
 
@@ -12,13 +13,15 @@ export default async function SubscriptionPage() {
   if (!userId) {
     redirect("/login");
   }
-
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
   return (
     <>
       <Navbar />
 
       <div className="space-y-6 p-6">
         <h1 className="text-2xl font-bold">Assinatura</h1>
+
         <div className="flex gap-6">
           <Card className="w-[450px]">
             <CardHeader className="border-b border-solid py-8">
@@ -42,8 +45,14 @@ export default async function SubscriptionPage() {
               </div>
             </CardContent>
           </Card>
+
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
